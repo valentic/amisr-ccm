8dcd08ea1c3c55256cbe8b740446d2f29cbcdc51
Todd Valentic <todd.valentic@gmail.com>
2023-10-05 09:16:11 -0700

    Update datatransport packge version

--- external/package/python-datatransport/python-datatransport.mk
+++ external/package/python-datatransport/python-datatransport.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_DATATRANSPORT_VERSION = 3.0.17
+PYTHON_DATATRANSPORT_VERSION = 3.0.18
 PYTHON_DATATRANSPORT_SOURCE = datatransport-$(PYTHON_DATATRANSPORT_VERSION).tar.gz
-PYTHON_DATATRANSPORT_SITE = https://files.pythonhosted.org/packages/29/a3/d69669a780197410f13d8c8697502b9c1503408e063ce84c4ed731c948f2
+PYTHON_DATATRANSPORT_SITE = https://files.pythonhosted.org/packages/4c/e0/17db1b4141aff02c4cfeaf6a231bdac02ab573ec7d9875b77a0bebe90ad5
 PYTHON_DATATRANSPORT_SETUP_TYPE = setuptools
 PYTHON_DATATRANSPORT_LICENSE = GNU General Public License v3 (GPLv3)
 PYTHON_DATATRANSPORT_LICENSE_FILES = LICENSE
