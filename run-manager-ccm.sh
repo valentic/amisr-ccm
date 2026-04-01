078170d14b936153f3ac417e4e7b1bdda284e320
Todd Valentic <todd.valentic@gmail.com>
2026-03-31 17:23:19 -0700

    Add container build system

--- run-manager-ccm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-./manager \
-    -v \
-    -p amisr-ccm-v2 \
-    -b 2023.05.1 \
-    setup 
-
--- run-manager-gateway.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-
-./manager \
-    -v \
-    -p amisr-gateway-v1 \
-    -b 2023.02.1 \
-    setup 
-
